0eba7ace9599d461ac56f2eab8fe18b0856d301a
sefaapinar <sefanur.pinar@gmail.com>
2021-10-27 11:04:49 +0300

    10/27/21

--- DataAccess/EntityFramework/EFContentDal.cs
+++ DataAccess/EntityFramework/EFContentDal.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccess.Abstract;
+using DataAccess.Concrete.Repositories;
+using Entity.concrete;
 
 namespace DataAccess.EntityFramework
 {
-    public class EFContentDal
+    public class EFContentDal: GenericRepository<Content>,IContentDal
     {
+
     }
 }
